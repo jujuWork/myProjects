8098ad7438dd41b73b7b97b3542e8b86043be8fd
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2026-01-30 22:30:51 +0900

    old js

--- Third/index.js
+++ Third/index.js
@@ -1,17 +1,3 @@
-// VERTICAL SCROLLING FOR BACKGROUND
-
-// const img2 = document.querySelectorAll(".main__img")[1];
-
-// window.addEventListener("scroll", () => {
-//   const scrolled = window.pageYOffset;
-//   const maxScroll = document.body.scrollHeight - window.innerHeight;
-//   const scrollPercent = scrolled / maxScroll;
-
-//   // Move second image from bottom (100%) to top (0%)
-//   const translateY = 100 - scrollPercent * 100;
-//   img2.style.transform = `translateY(-${100 - translateY}%)`;
-// });
-
 const img2 = document.querySelectorAll(".main__img")[1];
 const carouselWrapper = document.querySelector(".case__carousel-wrapper");
 const carouselSwiper = document.querySelector(".case__carousel-swiper");
